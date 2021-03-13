074ea7ba075b03361839ee0820ed55ff2502ab0f
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-13 16:24:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pract_3_eje_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y = 0;
+            
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("SUMA DE NUMEROS ENTEROS" + "\n");
+                Console.WriteLine("Inserte un numero positivo: (digite 0 para terminar)");
+                x = Convert.ToInt32(Console.ReadLine());
+                y = x + y;
+
+            } while (x != 0);
+
+            Console.Clear();
+            Console.WriteLine("SUMA DE NUMEROS ENTEROS" + "\n");
+            Console.WriteLine("RESULTADO: " + y);
+            Console.ReadKey();
+
+        }
+    }
+}
